f71cbc72cf3bcdc191a1a075f0daf4b150b7f907
Nicole Montes <nikolemontes@gmail.com>
2023-02-28 17:50:36 -0400

    ProductSlice && Component

--- client/app/store.js
+++ client/app/store.js
@@ -1,10 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import authReducer from '../slices/loginSlice';
+import allProductsSlice from '../slices/productSlice';
+
+
 
 const store = configureStore({
-  reducer: { auth: authReducer },
+  reducer: { 
+    auth: authReducer, 
+    allProducts: allProductsSlice,
+   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  
 });
 
 export default store;
---
+++ server/db/models/Product.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize')
+const db = require("../db");
+
+const Product = db.define("product", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  imageURL: {
+    type: Sequelize.STRING,
+    defaultValue: 'https://images.pexels.com/photos/699122/pexels-photo-699122.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  },
+});
+
+module.exports = Product
