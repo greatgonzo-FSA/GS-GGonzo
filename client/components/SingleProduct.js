c96ad3caab278e7b240938ea111b63e08466b2fe
what <what@cabin.home>
2023-03-06 17:05:50 -0500

    single product renders

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -7,16 +7,18 @@ const SingleProduct = () => {
 
     const dispatch = useDispatch()
     const { productId } = useParams()
-    const singleProduct = useSelector(selectSingleProduct)
+    const singleProduct = useSelector((state) => selectSingleProduct(state, productId))
     const { brand ,model, price, description, imageUrl } = singleProduct
 
     useEffect(() => {
-        dispatch(fetchSingleProductAsync (productId))
+        dispatch(fetchSingleProductAsync(productId))
     }, [dispatch])
+    console.log(singleProduct, "DATTTAAAAAA")
 
     return (
         <div id='single-product'>
-            <h2>{brand}</h2>
+            <h2>{model}</h2>
+            <p>{description}</p>
         </div>
     )
 }
